2eb0333557cabab02156347cef44e0c9749d8a1e
zrp <109859995+zhao5941741@users.noreply.github.com>
2022-07-26 23:08:14 +0800

    Changes

---
+++ app/src/main/java/com/example/znp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.znp;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //根据ID查询view
+        View view = findViewById(R.id.mainTextView);
+    }
+}
